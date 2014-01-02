928a1c48aa5ddb4a55e36126027cf5fd3d8335b3
Nicolas McCurdy <thenickperson@gmail.com>
2014-01-02 05:54:22 -0500

    Inline the urlForLibrary function

--- helpers.js
+++ helpers.js
@@ -3,15 +3,11 @@
 var http = require("http");
 var packages = require("./tmp/packages.json").packages;
 
-exports.urlForLibrary = function (library, file, version) {
-  return "//cdnjs.cloudflare.com/ajax/libs/" + library + "/" + version + "/" + file;
-};
-
 exports.getLibraryUrls = function (callback) {
   var result = packages.map(function (data) {
     return {
       name: data.name,
-      url: exports.urlForLibrary(data.name, data.filename, data.version)
+      url: "//cdnjs.cloudflare.com/ajax/libs/" + data.name + "/" + data.version + "/" + data.filename
     }
   });
   callback(result);
