ddf0bccb981ca5bb6ebfb21ea17b761099148314
Nicolas McCurdy <thenickperson@gmail.com>
2014-01-03 00:59:38 -0500

    Rename the library variable to activeLibrary

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,6 @@ exports.index = function(req, res){
   res.render('index', {
     title: 'JS Box',
     libraries: helpers.getLibraries(),
-    library: req.query.library ? JSON.parse(req.query.library) : undefined
+    activeLibrary: req.query.library ? JSON.parse(req.query.library) : undefined
   });
 };
