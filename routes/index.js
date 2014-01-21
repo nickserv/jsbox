e3b7b7a916e49b4938021c2fe842125dc4160da7
Nicolas McCurdy <thenickperson@gmail.com>
2014-01-21 00:08:54 -0500

    Show active libraries in the library chooser when the page loads

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@
  * GET home page.
  */
 
+var _ = require('underscore');
 var helpers = require('../helpers');
 
 exports.index = function(req, res){
   res.render('index', {
     title: 'JS Box',
     libraries: helpers.getLibraries(),
-    activeLibraries: helpers.getActiveLibraries(req.query.libraries)
+    activeLibraries: helpers.getActiveLibraries(req.query.libraries),
+    isActiveLibrary: function (library, activeLibraries) {
+      return _.contains(_.pluck(activeLibraries, 'name'), library.name);
+    }
   });
 };
