ac2562f07a36c430fd8646b586dbd8448a85c546
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-14 16:12:49 -0400

    Don't use $(document).ready to fix an issue

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,4 @@
-$(document).ready(function () {
-  // Set up Chosen for the main select field
-  $(".chosen-select").chosen({
-    placeholder_text_multiple: "select some libraries"
-  });
+// Set up Chosen for the main select field
+$(".chosen-select").chosen({
+  placeholder_text_multiple: "select some libraries"
 });
