59c7c2946d5c4db36cae1b678c1284fa99185ea6
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-14 16:25:57 -0400

    Migrate to Express 4

--- app.js
+++ app.js
@@ -15,18 +15,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 // Routes
 app.get('/', routes.index);
